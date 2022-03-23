17ec6f4990a3b1bd7e7e6af725f80ceb00a8725b
Overflowy <overflowy@pm.me>
2022-03-23 09:49:56 +0100

    Add timer decorator

--- f_util/__init__.py
+++ f_util/__init__.py
@@ -1,4 +1,22 @@
-# String manipulation
+import functools
+import time
+
+
+def timer(func):
+    """Used for measuring the execution time."""
+
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        tic = time.perf_counter()
+        value = func(*args, **kwargs)
+        toc = time.perf_counter()
+        elapsed_time = toc - tic
+        print(f"{func.__class__}.{func.__name__}: {elapsed_time:0.5f} seconds")
+        return value
+
+    return wrapper_timer
+
+
 def replace_multiple(s, *args):
     """Usage:
     >>> replace_multiple('hello', ('h', 'j'), ('e', 'ee'))
